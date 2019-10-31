f7bb52e43c86a8ac9810c7a49ab5b457b8cadeec
boruch d <dubywider@gmail.com>
2019-10-31 16:42:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,5 +6,14 @@ attr_reader :id
     @name = name
     @grade = grade
   end
-  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
 end
