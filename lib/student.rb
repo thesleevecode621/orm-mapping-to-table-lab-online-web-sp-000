4b11b3e6cd0ebe305ebebcc69ae78edde527691d
boruch d <dubywider@gmail.com>
2019-10-31 16:41:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,9 @@
 class Student
 attr_accessor :name,:grade
 attr_reader :id 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @album = album
+  end
 end
